171b2e815c056b02f4c835ec3767cb72a854030a
Gareth Palmer <bravdthepally@gmail.com>
2022-10-16 09:59:42 +0200

    Revamped as a static method #1

--- src/JSONTool.py
+++ src/JSONTool.py
@@ -1,23 +1,19 @@
 class JSON:
-    def __init__(self, data: dict) -> None:
-        self.data = data
+    @staticmethod
+    def dump_to_file(data: dict|list, file_name: str = 'results', pretty_print: bool = False) -> None:
+        """Export a specified dictionary or list to a JSON file.
 
-
-    def write_file(self, file_name: str = 'results', pretty_print: bool = False) -> object:
-        """Export the completed map to a JSON file.
         Args:
-            file_name (str, optional): The name of the file to export to. Extension can be left off. Defaults to 'data'.
+            data (dict | list): The data to write to the JSON file
+            file_name (str, optional): The name of the file to export to. Extension can be left off. Defaults to 'results'.
             pretty_print (bool, optional): Format the JSON to be more readable. False will leave the JSON at it's most compact. Defaults to False.
-        Returns:
-            object: self - used for method chaining.
         """
         import json
         if file_name[-5:] != '.json':
             file_name = str(file_name) + '.json'
         with open(file_name, "w") as file:
             if pretty_print:
-                json.dump(self.data, file, indent=4)
+                json.dump(data, file, indent=4)
             else:
-                json.dump(self.data, file)
+                json.dump(data, file)
         file.close()
-        return self
